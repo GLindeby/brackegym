b4c3188ca866c202e16a7da080473c02ca61bdae
Rasmus <rasmus_lindy@hotmail.com>
2018-05-05 20:39:30 +0200

    WUUUAAAHHgit status

--- client/src/app/services/profileservice.service.ts
+++ client/src/app/services/profileservice.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from "rxjs/Rx";
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileserviceService {
 
-  constructor() { }
+  constructor(private http : Http) { }
+  
+  
+  getProfiles(){
+
+    console.log();
+    
+    return this.http.get("http://localhost:3000/api/user/").
+      map(response => response.json());
+      
+    
+  }
+  addProfile(){
+  
+  }  
 }
+
