844a37a9846981fe7a329c5032fe14185d545842
Rasmus <rasmus_lindy@hotmail.com>
2018-05-05 23:19:59 +0200

    resolved merge conflict

--- client/src/app/services/profileservice.service.ts
+++ client/src/app/services/profileservice.service.ts
@@ -11,10 +11,7 @@ export class ProfileserviceService {
   
   
   getProfiles(){  
-<<<<<<< HEAD
     
-=======
->>>>>>> a410e083f79975ba76af9243382326d9d5464e24
     return this.http.get("http://localhost:3000/api/user/all");
 
   }
