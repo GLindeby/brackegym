6486c370c9fc2a9b023a1fc0f07bb7b56cca8d86
Isak Einler Larsson <isakeinlerlarsson@Isaks-MacBook-Pro.local>
2018-05-05 19:51:26 +0200

    fixade navbaren och första fielden i formen

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProfileComponent} from './profile/profile.component';
 import {MakeprofileComponent} from './makeprofile/makeprofile.component';
+import {MatchComponent} from './match/match.component';
 
 const routes: Routes = [
   {path: 'profile', component: ProfileComponent},
-  {path: 'makeprofile' component: MakeprofileComponent}
+  {path: 'makeprofile', component: MakeprofileComponent},
+  {path: 'match', component: MatchComponent}
   //{path: 'makeprofile', component: MakeprofileComponent}
 ];
 
