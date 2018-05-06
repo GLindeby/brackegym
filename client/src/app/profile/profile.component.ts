89108dd4e5f5d4564f8afa71a9dcb9a61cde11c8
Gabriel Lindeby <gablinde@student.chalmers.se>
2018-05-06 11:23:47 +0200

    stars

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -30,11 +30,23 @@ export class ProfileComponent implements OnInit {
    this.profileservice.getProfile(name).subscribe(res => {
     this.profile = res[0] as Profile;
     console.log(this.profile);
-    this.numbers = Array(5).fill(0).map((x,i)=>{
-      console.log(this.profile.rating);
+    console.log(this.profile.rating);
+    this.numbers = Array(5);
+    for(var i = 0; i<this.numbers.length; i++){
+      if((i+1) <= +this.profile.rating){
+        this.numbers[i] = 1;
+      }else{
+        this.numbers[i] = 0;
+      }
     }
+    console.log(this.numbers);
+    /*this.numbers = Array(5).fill(0).map((x,i)=>{
+      numbers
+    }
+      +(this.profile.rating)
     );
     console.log(this.numbers);
+    });*/
     });
   }
 
